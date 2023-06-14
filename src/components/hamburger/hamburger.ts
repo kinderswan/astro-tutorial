b50f835931ab71a70bdc9fd12c55a9ee3025d793
Pavel Hamanovich <hamanovichpd@oxagile.com>
2023-06-14 18:30:15 +0300

    Build a blog page

---
+++ src/components/hamburger/hamburger.ts
@@ -0,0 +1,5 @@
+export const addMenu = () => {
+  document.querySelector('.hamburger')?.addEventListener('click', () => {
+    document.querySelector('.nav-links')?.classList.toggle('expanded')
+  })
+}
